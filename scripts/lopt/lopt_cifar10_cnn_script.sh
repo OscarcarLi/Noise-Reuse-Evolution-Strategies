3570f080b42e18b8da66ebb64d33f8ad42944cb6
Oscar Li <oscarliduke@gmail.com>
2023-12-21 17:09:03 -0500

    Add missing `--use_threshold_outer_cross_entropy_loss` flag for cifar learned optimizer.

--- scripts/lopt/lopt_cifar10_cnn_script.sh
+++ scripts/lopt/lopt_cifar10_cnn_script.sh
@@ -15,7 +15,7 @@ seed=41
 batch_size=32; init_seed=5; N=10; W=100; activation="relu"; lr="3.0e-4"; device=7; outer_iterations=20000
 
 # set a seed and its config in command line before running the following command
-  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator TruncatedPESK --num_particles $N --num_gradient_estimators 1 --trunc_length $W --K 1001 --sigma 0.01 --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
+  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --use_threshold_outer_cross_entropy_loss --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator TruncatedPESK --num_particles $N --num_gradient_estimators 1 --trunc_length $W --K 1001 --sigma 0.01 --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
 
 # PES
 seed=23
@@ -29,7 +29,7 @@ batch_size=32; init_seed=5; N=10; W=100; activation="relu"; lr="3.0e-4"; device=
 seed=41
 batch_size=32; init_seed=5; N=10; W=100; activation="relu"; lr="3.0e-4"; device=1; outer_iterations=20000
 
-  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator TruncatedPESK --num_particles $N --num_gradient_estimators 1 --trunc_length $W --K $W --sigma 0.01 --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
+  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --use_threshold_outer_cross_entropy_loss --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator TruncatedPESK --num_particles $N --num_gradient_estimators 1 --trunc_length $W --K $W --sigma 0.01 --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
 
 # TES
 seed=23
@@ -43,7 +43,7 @@ batch_size=32; init_seed=5; N=10; W=100; activation="relu"; lr="1.0e-3"; device=
 seed=41
 batch_size=32; init_seed=5; N=10; W=100; activation="relu"; lr="1.0e-3"; device=5; outer_iterations=20000
 
-  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator TruncatedESBiased --num_particles $N --num_gradient_estimators 1 --trunc_length $W --sigma 0.01 --noloss_normalize --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
+  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --use_threshold_outer_cross_entropy_loss --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator TruncatedESBiased --num_particles $N --num_gradient_estimators 1 --trunc_length $W --sigma 0.01 --noloss_normalize --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
 
 # FullES
 seed=23
@@ -57,4 +57,4 @@ batch_size=32; init_seed=5; N=1; activation="relu"; lr="1.0e-3"; device=6; outer
 seed=41
 batch_size=32; init_seed=5; N=1; activation="relu"; lr="1.0e-3"; device=7; outer_iterations=20000
 
-  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator FullES --num_particles $N --num_gradient_estimators 1 --sigma 0.01 --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
+  CUDA_VISIBLE_DEVICES=$device python3 main_training.py --application lopt --horizon_length 1000 --lopt_dataset_name 'cifar10' --image_length 32 --lopt_batch_size $batch_size --use_val --nouse_accuracy --use_threshold_outer_cross_entropy_loss --lopt_inner_model cnn --lopt_activation $activation --lopt_hidden_channels "32,32,32,32" --lopt_architecture MLPLOpt --lopt_single_sequence_seed 66 --gradient_estimator FullES --num_particles $N --num_gradient_estimators 1 --sigma 0.01 --outer_optimizer_specs "ADAM||CONSTANT$lr" --outer_iterations $outer_iterations --init_seed $init_seed --remaining_seed $seed --run_test_during_training --norun_test_after_training --eval_freq 10;
